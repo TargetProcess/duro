f13783e09e20f4c495478c15caf79cef1d048d47
Jury Razumau <jurazumau@gmail.com>
2017-03-16 20:39:36 +0300

    update type hints for graph utils

--- graph_utils.py
+++ graph_utils.py
@@ -1,15 +1,16 @@
 import networkx as nx
+from typing import List
 
 
-def find_roots_without_interval(graph: nx.DiGraph) -> list:
+def find_roots_without_interval(graph: nx.DiGraph) -> List[str]:
     return find_sources_without_attribute(graph, 'interval')
 
 
-def find_sources(graph: nx.DiGraph) -> list:
+def find_sources(graph: nx.DiGraph) -> List[str]:
     return [node for (node, in_degree) in graph.in_degree().items() if in_degree == 0]
 
 
-def find_sources_without_attribute(graph: nx.DiGraph, attribute: str) -> list:
+def find_sources_without_attribute(graph: nx.DiGraph, attribute: str) -> List[str]:
     roots = find_sources(graph)
     nodes = dict(graph.nodes(data=True))
     return [root for root in roots if not nodes[root].get(attribute)]
