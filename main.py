7fa932f81286be3d7e87a24605002bd5ca7028bf
Jury Razumau <jurazumau@gmail.com>
2017-04-16 21:58:17 +0300

    write graph to dot file, save data to db

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import networkx as nx
-from graph_utils import find_roots_without_interval, detect_cycles
-from file_utils import list_view_files
-from errors import NotADAGError, RootsWithoutIntervalError, MaterializationError
-from pprint import pprint
 import re
+from pprint import pprint
+import sqlite3
+
+import networkx as nx
+
+from errors import NotADAGError, RootsWithoutIntervalError, MaterializationError
+from file_utils import list_view_files
+from graph_utils import find_roots_without_interval, detect_cycles, copy_graph_without_attributes
+# from schedule_ansible import build_single_schedule
+from schedule_cron import build_single_schedule
 
 
 def build_graph(folder: str) -> nx.DiGraph:
     graph = nx.DiGraph()
-    # pprint(list_view_files(folder))
     graph.add_nodes_from(list_view_files(folder))
     nodes_list = graph.nodes()
     for node, query in graph.nodes_iter(data=True):
         for other_node in nodes_list:
-            if re.search(r'\b' + re.escape(other_node) + r'\b', query.get('contents')):
+            if re.search(r'\b' + re.escape(other_node) + r'\b',
+                         query.get('contents')):
                 graph.add_edge(node, other_node)
     return graph
 
@@ -27,27 +32,43 @@ def draw_subgraphs(graph: nx.DiGraph):
 
 
 def create_schedule(graph: nx.DiGraph, yml_file: str):
-    schedule = ''
+    nodes = dict(graph.nodes(data=True))
+    nodes_with_interval = ((k, v['interval']) for k, v in nodes.items() if
+                           v.get('interval'))
+    schedule = '\n'.join((build_single_schedule(table, interval)
+                          for (table, interval) in nodes_with_interval))
 
     with open(yml_file, 'w') as file:
         file.write(schedule)
 
 
-def main(sql_folder, yml_file, strict=False):
+def save_to_db(graph: nx.DiGraph, db_path: str):
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    # TODO: migrate data, do not drop table
+    cursor.execute('''DROP TABLE IF EXISTS tables''')
+    cursor.execute('''CREATE TABLE tables
+                        (table_name text, query text, last_created integer);''')
+
+    nodes = dict(graph.nodes(data=True))
+    tables_and_queries = [(k, v['contents'], None) for k, v in nodes.items()]
+
+    cursor.executemany('''INSERT INTO tables VALUES (?, ?, ?)''', tables_and_queries)
+    connection.commit()
+    connection.close()
+
+
+def main(sql_folder, yml_file, strict=False, db_path='duro.db'):
     graph = build_graph(sql_folder)
     is_dag, cycles = detect_cycles(graph, strict)
     nx.nx_pydot.to_pydot(graph).write_png('dependencies.png')
-    # nx.nx_pydot.write_dot(graph, 'dependencies.dot')
+    nx.nx_pydot.write_dot(copy_graph_without_attributes(graph, ['contents', 'interval']), 'dependencies.dot')
 
     if not is_dag:
         print('Views dependency graph is not a DAG. Cycles detected:')
         for cycle in cycles:
             print(cycle)
         raise NotADAGError
-
-
-
-    # draw_subgraphs(graph)
 
     roots_without_interval = find_roots_without_interval(graph)
 
@@ -57,6 +78,7 @@ def main(sql_folder, yml_file, strict=False):
         raise RootsWithoutIntervalError
 
     create_schedule(graph, yml_file)
+    save_to_db(graph, db_path)
 
 
 if __name__ == '__main__':
