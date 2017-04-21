37296025fb5525000138bdc84a315ead9bf78e7f
Jury Razumau <jurazumau@gmail.com>
2017-04-21 20:50:21 +0300

    read credentials from config

--- credentials.py
+++ credentials.py
@@ -1,5 +1,13 @@
 from typing import Dict
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.conf')
 
 
 def redshift_credentials() -> Dict:
-    return {}
+    return dict(config['redshift'])
+
+
+def slack_credentials() -> Dict:
+    return dict(config['slack'])
