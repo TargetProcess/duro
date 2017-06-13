11f4eabd9f97e99aba49f9a3ecd231764c6a8356
Jury Razumau <jurazumau@gmail.com>
2017-06-13 13:11:59 +0300

    use inherited templates

---
+++ server/static/tableDetails.js
@@ -0,0 +1,26 @@
+var chart = new tauCharts.Chart({
+    data: datasource,
+    type: 'line',
+    x: 'date',
+    y: 'duration',
+    dimensions: {
+        date: {
+        type: 'measure',
+        scale: 'time'
+        },
+        duration: { type: 'measure'}
+    },
+    guide: {
+        x: {label: 'Creation start'},
+        y: {label: 'Creation duration'},
+        interpolate: 'smooth-keep-extremum',
+        showAnchors: 'always'
+
+    },
+    plugins: [
+        tauCharts.api.plugins.get('tooltip')(),
+        tauCharts.api.plugins.get('legend')()
+
+    ]
+});
+chart.renderTo('#chart');
