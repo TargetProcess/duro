2f425312222222830cd0807da3427499f2bd23b8
Alexander Zaitsev <alexander.zaitsev@targetprocess.com>
2017-07-14 18:15:53 +0300

    debug mode

--- create_single_table.py
+++ create_single_table.py
@@ -11,13 +11,18 @@ from utils import Table
 import argparse
 
 
-def create_table(table: Table, views_path: str):
+def create_table(table: Table, views_path: str, verbose=False):
     config = load_config(table.name, views_path)
     print(f'Creating {table.name}')
+    if verbose:
+        print(f'Using views path: {views_path}')
 
     connection = create_connection()
 
     processor = load_processor(table.name, views_path)
+
+    if verbose:
+        print(f'Loaded processor: {processor}')
     if processor:
         process_and_upload_data(table, processor,
                                 connection, config,
@@ -41,7 +46,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('table', help='table to create', type=str)
     parser.add_argument('--path', '-p', default='../materialized-views/', help='folder containing the views')
+    parser.add_argument('--verbose', '-v', default=False, help='Verbose', action='store_true')
     args = parser.parse_args()
     table = Table(args.table, load_query(args.table, args.path), None, None)
-    create_table(table, args.path)
+    create_table(table, args.path, args.verbose)
 
